efc467bd70bfd1e87468789524a215c1f0357f3d
Ksantus <99861100+Ksantus@users.noreply.github.com>
2025-07-07 20:08:58 +0300

    Add files via upload

---
+++ pages/setting.py
@@ -0,0 +1,10 @@
+valid_email = 'evgenjamar@mail.ru'
+valid_phone = '+7(926)9390545'
+valid_password = 'poland2012'
+valid_login = 'evgenjamar'
+valid_ls = '123456789123'
+valid_username = u"Тестов Ромашка"
+ad_slogan = u"Персональный помощник в цифровом мире Ростелекома"
+site = 'https://b2c.passport.rt.ru'
+link_lk = "/account_b2c/page"
+drv_path = '../../ChromeDrv/chromedriver.exe'
